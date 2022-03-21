ada92c382abe462d73824a859763cfdd4f0ea0ce
wole <wojciech.olejnik@inetum.com>
2022-03-21 12:44:07 +0100

    add tooltip

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -2,8 +2,11 @@ const User = require('../models/user.model');
   exports.login = async (req, res) => {
     try {
       const user = await User.find({email: req.params.email, password: req.params.password});
-      if(!user) res.status(404).json({ message: 'Not found' });
-      else res.json(user);
+      if (!user.length) {
+        res.status(404).json({ message: 'invalid data'});
+      } else {
+        res.json(user);
+      }
     }
     catch(err) {
       res.status(500).json({ message: err });
